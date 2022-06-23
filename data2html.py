d9de4c9179544dc2dd7da7b36893907b7cdd9061
zxy <zxy@pc-acer>
2022-06-23 16:41:34 +0800

    scrape more fields

--- data2html.py
+++ data2html.py
@@ -1,5 +1,6 @@
 import json
 import sys
+from html import escape
 
 html_template = '''
 <!DOCTYPE html>
@@ -8,7 +9,48 @@ html_template = '''
         <title>output</title>
     </head>
     <body>
+        <h2>Output</h2>
+        <div>webgraphics script begin</div>
+        <script src="https://pmn.plantcyc.org/webgraphics.js"></script>
+        <div>webgraphics script end</div>
+        <script>
+            async function renderAllWG() {
+                let elems = document.getElementsByClassName('wg-div');
+                for (let elem of elems) {
+                    let id = elem.id;
+                    let b64 = btoa(elem.getAttribute('data-wg'));
+                    let dataUrl = 'data:application/json;base64,' + b64;
+                    WG.Load(dataUrl, id);
+                }
+            }
+        </script>
+        <div>
+            <button onclick="renderAllWG()">Click here to render all image using plantcyc's webgraphics library</button>
+        </div>
+
+        <style>
+            td {
+                border: 1px solid black;
+            }
+        </style>
+
         <table>
+            <thead>
+                <td>No.</td>
+                <td>id</td>
+                <td>name</td>
+                <td>formula</td>
+                <td>synonyms</td>
+                <td>smiles</td>
+                <td>inchi</td>
+                <td>summary</td>
+                <td>chebi</td>
+                <td>kegg</td>
+                <td>pubchem</td>
+                <td>refmet</td>
+                <td>image</td>
+                <td>url</td>
+            </thead>
             <tbody>
                 %s
             </tbody>
@@ -24,11 +66,23 @@ def main():
     with open(sys.argv[1], 'r', encoding='utf-8') as f:
         data = json.load(f)
     trs = []
+    i = 0
     for id_ in data:
+        i += 1
         tr = f'''<tr>
+            <td>{i}</td>
             <td>{id_}</td>
             <td>{data[id_]["name"]}</td>
             <td>{data[id_]["formula"]}</td>
+            <td>{data[id_]["synonyms"]}</td>
+            <td>{data[id_]["smiles"]}</td>
+            <td>{data[id_]["inchi"]}</td>
+            <td>{data[id_]["summary"]}</td>
+            <td>{data[id_]["chebi"]}</td>
+            <td>{data[id_]["kegg"]}</td>
+            <td>{data[id_]["pubchem"]}</td>
+            <td>{data[id_]["refmet"]}</td>
+            <td><div class='wg-div' id="wg-{id_}" data-wg="{escape(data[id_]["webgraphics_data"])}"></div></td>
             <td> <a href="{data[id_]["url"]}">{data[id_]["url"]}</a></td>
         </tr>'''
         trs.append(tr)
--- plantcyc_scraper.py
+++ plantcyc_scraper.py
@@ -7,15 +7,72 @@ import requests
 import urllib.parse
 from bs4 import BeautifulSoup
 
-# os.environ['http_proxy'] = 'http://127.0.0.1:10801'
-# os.environ['https_proxy'] = 'http://127.0.0.1:10801'
+os.environ['http_proxy'] = 'http://127.0.0.1:10801'
+os.environ['https_proxy'] = 'http://127.0.0.1:10801'
 
 def make_url(id_):
     id_ = urllib.parse.quote_plus(id_)
     return f'https://pmn.plantcyc.org/compound?orgid=PLANT&id={id_}'
 
-def make_valud_filename(name):
-    return re.sub(r':', '_', name)
+def make_summary_url(id_):
+    id_ = urllib.parse.quote_plus(id_)
+    return f'https://pmn.plantcyc.org/cpd-tab?orgid=PLANT&id={id_}&tab=SUMMARY'
+
+def make_wg_url(path):
+    return 'https://pmn.plantcyc.org' + path
+
+def make_valid_filename(name):
+    return re.sub(r':|\/', '_', name)
+
+def get_table_row_content(soup, row_name):
+    res = soup.find_all('td', string=row_name)
+    if (len(res)) == 0:
+        value = ''
+    else:
+        elem = res[0]
+        elem = elem.find_next_sibling()
+        value = elem.decode_contents()
+        value = value.strip()
+    return value
+
+def get_page(ses, ns, id_, **extra):
+    reload = extra.get('reload', False)
+    if not ns:
+        url = make_url(id_)
+    elif ns == 'summary':
+        url = make_summary_url(id_)
+    elif ns == 'webgraphics':
+        url = make_wg_url(extra['path'])
+        # print(f'wg url is: {url}')
+    else:
+        raise NotImplementedError()
+    
+    filename = make_valid_filename(id_) + '.html'
+    page_dir = 'pages'
+    if ns:
+        page_dir += '/' + ns
+    try:
+        os.makedirs(page_dir)
+    except FileExistsError:
+        pass
+    page_file = f'{page_dir}/{filename}'
+    if os.path.exists(page_file) and not reload:
+        with open(page_file, encoding='utf-8') as f:
+            content = f.read()
+    else:
+        if ns == 'webgraphics':
+            # we have to request the main page so that server generates webgraphics data
+            get_page(ses, None, id_, reload=True)
+        res = ses.get(url)
+        if not res.ok:
+            print(res.text)
+            print('url: ', res.url)
+            raise ValueError(f'Server returned a bad response: {res}')
+        else:
+            content = res.text
+            with open(page_file, 'w', encoding='utf-8') as f:
+                f.write(content)
+    return content
 
 def main():
     if len(sys.argv) != 2:
@@ -42,26 +99,16 @@ def main():
             # print(f'handling {id_}')
             if id_ in data:
                 continue
+            print(f'processing {id_}...')
             url = make_url(id_)
-            filename = make_valud_filename(id_) + '.html'
-            page_file = f'pages/{filename}'
-            if os.path.exists(page_file):
-                with open(page_file, encoding='utf-8') as f:
-                    content = f.read()
-            else:
-                res = ses.get(url)
-                if not res.ok:
-                    print(res.text)
-                    raise ValueError(f'Server returned a bad response: {res}')
-                else:
-                    content = res.text
-                    with open(page_file, 'w', encoding='utf-8') as f:
-                        f.write(content)
+            content = get_page(ses, None, id_)
             soup = BeautifulSoup(content, 'html.parser')
+
             res = soup.find_all('font', class_='header')
             elem = res[0]
             name = elem.decode_contents()
             name = name.strip()
+
             res = soup.find_all('td', string='\nChemical Formula')
             if (len(res)) == 0:
                 formula = ''
@@ -70,14 +117,57 @@ def main():
                 elem = elem.find_next_sibling()
                 formula = elem.decode_contents()
                 formula = formula.strip()
-            print(name, formula)
+            
+            summary_content = get_page(ses, 'summary', id_)
+            summary_soup = BeautifulSoup(summary_content, 'html.parser')
+            res = summary_soup.find_all('td', string='\nSynonyms')
+            if len(res) == 0:
+                synonyms = ''
+            else:
+                elem = res[0]
+                elem = elem.find_next_sibling()
+                synonyms = elem.decode_contents()
+                synonyms = synonyms.strip()
+
+            smiles = get_table_row_content(summary_soup, '\nSMILES')
+            inchi = get_table_row_content(summary_soup, '\nInChI')
+
+            res = summary_soup.select('.summaryText h3')
+            if len(res) == 0:
+                summary = ''
+            else:
+                elem = res[0]
+                elem = elem.next_sibling
+                summary = elem.strip()
+
+            chebi = get_table_row_content(summary_soup, '\nChEBI')
+            kegg = get_table_row_content(summary_soup, '\nKegg')
+            pubchem = get_table_row_content(summary_soup, '\nPubChem')
+            refmet = get_table_row_content(summary_soup, '\nRefMet')
+
+            res = re.search(r'\/\S+\.wg', content)
+            if not res:
+                webgraphics_data = ''
+            else:
+                wg_path = res.group(0)
+                webgraphics_data = get_page(ses, 'webgraphics', id_, path=wg_path)
+
             data[id_] = {
                 'name': name,
                 'formula': formula,
+                'synonyms': synonyms,
+                'smiles': smiles,
+                'inchi': inchi,
+                'summary': summary,
+                'chebi': chebi,
+                'kegg': kegg,
+                'pubchem': pubchem,
+                'refmet': refmet,
+                'webgraphics_data': webgraphics_data,
                 'url': url
             }
             print(f'{id_}: {name} {formula}')
-            time.sleep(0.5)
+            # time.sleep(0.5)
             # break
     finally:
         with open('data.json', 'w', encoding='utf-8')as f:
